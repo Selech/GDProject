c3987862de7c1432efaed854f12e583bf3862027
Selech <selech@live.dk>
2015-03-24 15:16:55 +0100

    Update to controls, asteroid and project settings.

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -25,100 +25,35 @@ public class PlayerControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = Vector3.MoveTowards (transform.position,new Vector3(0,0,0),0.01f); 
+		transform.position = Vector3.MoveTowards (transform.position, new Vector3 (0, 0, 0), 0.01f); 
 		
 		//asteroidsControl ();
 		physicsControl ();
 		//translateControl ();
 	}
 
-	public void asteroidsControl(){
-		//transform.rotation = Vector3.RotateTowards(transform.position,target.transform.position,2f,2f);
+	public void physicsControl(){
+		//print ("Physics controls");
 
-		if (Input.GetKey (up)) {
-			Vector3 direction = ship.transform.position - target.transform.position;
-			print (direction);
-			GetComponent<Rigidbody>().AddForce(-direction*1.5f,ForceMode.Force);
-		}
-		
-		if (Input.GetKey (down)) {
-			Vector3 direction = ship.transform.position - target.transform.position;
-			print (direction);
-			GetComponent<Rigidbody>().AddForce(direction,ForceMode.Force);
-		}
-		
-		if (Input.GetKey (left)) {
-			print (target.transform.position.y - ship.transform.position.y);
-
-			if (PlayerNumber == 1 && (target.transform.position.y - ship.transform.position.y > -1)){
-				target.transform.Translate (new Vector3 (-0.12f, 0, 0));
-				ship.transform.Rotate (Vector3.back * 5);
-				ship.transform.Rotate (0,3f,0);
-			}
-			
-			if(PlayerNumber == 2 && (target.transform.position.y - ship.transform.position.y < 1) ){
-				target.transform.Translate(new Vector3(0.12f,0,0));
-				ship.transform.Rotate (Vector3.back * 5);
-				ship.transform.Rotate (0,-3f,0);
-			}
-		}
-		
-		if (Input.GetKey (right) ) {
-			if(PlayerNumber == 1 && (target.transform.position.y - ship.transform.position.y < 1)){
-				target.transform.Translate(new Vector3(0.12f,0,0));
-				ship.transform.Rotate(-Vector3.back * 5);
-				ship.transform.Rotate (0,-3f,0);
-
-			}
-
-			if(PlayerNumber == 2 && (target.transform.position.y - ship.transform.position.y > -1)){
-				target.transform.Translate(new Vector3(-0.12f,0,0));
-				ship.transform.Rotate(-Vector3.back * 5);
-				ship.transform.Rotate (0,3f,0);
-			}
-		}
-
-		// Shooting a bullet
+		//Single bullet
 		if(Input.GetKeyDown(shootBullet)){
 			Instantiate(bullet, ship.transform.position - (new Vector3(0.5f, 
-			           (ship.transform.position.y - target.transform.position.y) * 0.5f, 0f)), new Quaternion());
+			                                                           (ship.transform.position.y - target.transform.position.y) * 0.5f, 0f)), new Quaternion());
 		}
 
-		// Shooting double bullets
+		//Double bullet
 		if(Input.GetKeyDown(shootDoubleBullets)){
 			
 			// Bullet on the right side of the ship
 			Instantiate(doubleBullet, ship.transform.position - (new Vector3((float)ship.transform.rotation.y + 0.5f, 
-			         (ship.transform.position.y - target.transform.position.y) * 0.3f, 0f)), new Quaternion());
+			                                                                 (ship.transform.position.y - target.transform.position.y) * 0.3f, 0f)), new Quaternion());
 			
 			// Bullet on the left side of the ship
 			Instantiate(doubleBullet, ship.transform.position - (new Vector3((float)ship.transform.rotation.y - 0.5f, 
-			         (ship.transform.position.y - target.transform.position.y) * 0.3f, 0f)), new Quaternion());
-
+			                                                                 (ship.transform.position.y - target.transform.position.y) * 0.3f, 0f)), new Quaternion());
+			
 			print("Ship rotation: " + ship.transform.rotation);
 		}
-	}
-
-	public void translateControl(){
-		if (Input.GetKey (up)) {
-			transform.Translate(new Vector3(0,speed,0));
-		}
-
-		if (Input.GetKey (down)) {
-			transform.Translate(new Vector3(0,-speed,0));
-		}
-
-		if (Input.GetKey (left)) {
-			transform.Translate(new Vector3(-speed,0,0));
-		}
-
-		if (Input.GetKey (right)) {
-			transform.Translate(new Vector3(speed,0,0));
-		}
-	}
-
-	public void physicsControl(){
-		//print ("Physics controls");
 
 		if (Input.GetKey (right)) {
 			GetComponent<Rigidbody>().AddForce(new Vector3(0,speed,0),ForceMode.Force);
@@ -144,5 +79,90 @@ public class PlayerControl : MonoBehaviour {
 			if(PlayerNumber == 2){
 				GetComponent<Rigidbody>().AddForce(new Vector3(-speed*0.5f,0,0),ForceMode.Force);
 			} 		}
+	}
+
+	public void asteroidsControl(){
+		//transform.rotation = Vector3.RotateTowards(transform.position,target.transform.position,2f,2f);
+		
+		if (Input.GetKey (up)) {
+			Vector3 direction = ship.transform.position - target.transform.position;
+			print (direction);
+			GetComponent<Rigidbody>().AddForce(-direction*1.5f,ForceMode.Force);
+		}
+		
+		if (Input.GetKey (down)) {
+			Vector3 direction = ship.transform.position - target.transform.position;
+			print (direction);
+			GetComponent<Rigidbody>().AddForce(direction,ForceMode.Force);
+		}
+		
+		if (Input.GetKey (left)) {
+			print (target.transform.position.y - ship.transform.position.y);
+			
+			if (PlayerNumber == 1 && (target.transform.position.y - ship.transform.position.y > -1)){
+				target.transform.Translate (new Vector3 (-0.12f, 0, 0));
+				ship.transform.Rotate (Vector3.back * 5);
+				ship.transform.Rotate (0,3f,0);
+			}
+			
+			if(PlayerNumber == 2 && (target.transform.position.y - ship.transform.position.y < 1) ){
+				target.transform.Translate(new Vector3(0.12f,0,0));
+				ship.transform.Rotate (Vector3.back * 5);
+				ship.transform.Rotate (0,-3f,0);
+			}
+		}
+		
+		if (Input.GetKey (right) ) {
+			if(PlayerNumber == 1 && (target.transform.position.y - ship.transform.position.y < 1)){
+				target.transform.Translate(new Vector3(0.12f,0,0));
+				ship.transform.Rotate(-Vector3.back * 5);
+				ship.transform.Rotate (0,-3f,0);
+				
+			}
+			
+			if(PlayerNumber == 2 && (target.transform.position.y - ship.transform.position.y > -1)){
+				target.transform.Translate(new Vector3(-0.12f,0,0));
+				ship.transform.Rotate(-Vector3.back * 5);
+				ship.transform.Rotate (0,3f,0);
+			}
+		}
+		
+		// Shooting a bullet
+		if(Input.GetKeyDown(shootBullet)){
+			Instantiate(bullet, ship.transform.position - (new Vector3(0.5f, 
+			                                                           (ship.transform.position.y - target.transform.position.y) * 0.5f, 0f)), new Quaternion());
+		}
+		
+		// Shooting double bullets
+		if(Input.GetKeyDown(shootDoubleBullets)){
+			
+			// Bullet on the right side of the ship
+			Instantiate(doubleBullet, ship.transform.position - (new Vector3((float)ship.transform.rotation.y + 0.5f, 
+			                                                                 (ship.transform.position.y - target.transform.position.y) * 0.3f, 0f)), new Quaternion());
+			
+			// Bullet on the left side of the ship
+			Instantiate(doubleBullet, ship.transform.position - (new Vector3((float)ship.transform.rotation.y - 0.5f, 
+			                                                                 (ship.transform.position.y - target.transform.position.y) * 0.3f, 0f)), new Quaternion());
+			
+			print("Ship rotation: " + ship.transform.rotation);
+		}
+	}
+
+	public void translateControl(){
+		if (Input.GetKey (up)) {
+			transform.Translate(new Vector3(0,speed,0));
+		}
+		
+		if (Input.GetKey (down)) {
+			transform.Translate(new Vector3(0,-speed,0));
+		}
+		
+		if (Input.GetKey (left)) {
+			transform.Translate(new Vector3(-speed,0,0));
+		}
+		
+		if (Input.GetKey (right)) {
+			transform.Translate(new Vector3(speed,0,0));
+		}
 	}
 }
