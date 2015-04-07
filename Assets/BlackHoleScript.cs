07212ad08833c468d6c7dfc60a32c1c8d36b54a0
Selech <selech@live.dk>
2015-04-07 13:11:39 +0200

    Slow motion when players are close to blackhole!

--- Assets/BlackHoleScript.cs
+++ Assets/BlackHoleScript.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class BlackHoleScript : MonoBehaviour {
 
+	public GameObject playerLeft;
+	public GameObject playerRight;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,7 +13,15 @@ public class BlackHoleScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(playerLeft.activeSelf && playerRight.activeSelf){
+			if (Vector3.Distance (playerLeft.transform.position,this.transform.position) < 2f || Vector3.Distance (playerRight.transform.position,this.transform.position) < 2f) {
+				Time.timeScale = 0.5F;
+			} else {
+				Time.timeScale = 1.0F;
+				
+			}
+			Time.fixedDeltaTime = 0.02F * Time.timeScale;
+		}
 	}
 
 	void OnCollisionEnter(Collision other){
